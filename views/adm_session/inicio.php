64e3b88abd8bc33a9393c6e9bd687768aa2ac906
[user] <angel.martinez@ivitec.com.mx>
2022-11-15 15:59:52 -0600

    v0.2.1 Se inicializan catalogos

--- views/adm_session/inicio.php
+++ views/adm_session/inicio.php
@@ -1,4 +1,4 @@
-<?php /** @var tglobally\tg_cliente\controllers\controlador_adm_session $controlador */
+<?php /** @var \tglobally\tg_cat_sat\controllers\controlador_adm_session $controlador */
 
 use config\views;
 $url_assets = (new views())->url_assets;
@@ -8,7 +8,7 @@ $url_assets = (new views())->url_assets;
     <div class="row">
         <div class="col-md-12">
             <div class="cont_text_inicio">
-                <h1 class="h-side-title page-title page-title-big text-color-primary">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h1>
+                <h1 class="h-side-title page-title page-title-big text-color-primary">Hola, Nombre Completo</h1>
                 <h1 class="h-side-title page-title text-color-primary">Da click en la sección que deseas utilizar</h1>
             </div>
         </div>
@@ -18,17 +18,19 @@ $url_assets = (new views())->url_assets;
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="col-sm-2">
-                <a href="<?php echo $controlador->link_lista_tg_cte_tipo_alianza; ?>">
-                    <div class="cont_imagen_accion">
-                        <img src="<?php echo $url_assets; ?>img/inicio/imagen_2.jpg">
-                    </div>
-                    <div class="cont_text_accion">
-                        <h4 class="text_seccion">Cat Sat</h4>
-                        <h4 class="text_accion">Catologo</h4>
-                    </div>
-                </a>
-            </div>
+            <?php foreach ($controlador->links_catalogos as $indice => $valor): ?>
+                <div class="col-sm-2">
+                    <a href="<?php echo $valor["link"];?>">
+                        <div class="cont_imagen_accion">
+                            <img src="<?php echo $url_assets; ?>img/inicio/imagen_2.jpg">
+                        </div>
+                        <div class="cont_text_accion">
+                            <h4 class="text_seccion"><?php echo $valor["titulo"];?></h4>
+                            <h4 class="text_accion"><?php echo $valor["subtitulo"];?></h4>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
