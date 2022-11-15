906e8543954addc6a7b23b5526577e5f02d9d934
[user] <angel.martinez@ivitec.com.mx>
2022-11-15 13:45:44 -0600

    v0.1.0 Configuracion Inicial

---
+++ js/adm_session/inicio.js
@@ -0,0 +1,4 @@
+let menu_barras_js = 'vendor/gamboa.martin/js_base/src/menu_barras.js';
+document.write('<script src="'+menu_barras_js+'"></script>');
+
+
---
+++ views/adm_session/inicio.php
@@ -0,0 +1,34 @@
+<?php /** @var tglobally\tg_cliente\controllers\controlador_adm_session $controlador */
+
+use config\views;
+$url_assets = (new views())->url_assets;
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="cont_text_inicio">
+                <h1 class="h-side-title page-title page-title-big text-color-primary">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h1>
+                <h1 class="h-side-title page-title text-color-primary">Da click en la sección que deseas utilizar</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-sm-2">
+                <a href="<?php echo $controlador->link_lista_tg_cte_tipo_alianza; ?>">
+                    <div class="cont_imagen_accion">
+                        <img src="<?php echo $url_assets; ?>img/inicio/imagen_2.jpg">
+                    </div>
+                    <div class="cont_text_accion">
+                        <h4 class="text_seccion">Cat Sat</h4>
+                        <h4 class="text_accion">Catologo</h4>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
