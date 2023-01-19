e8908ed927e8d5a43655aa6a036b88830729226a
[user] <angel.martinez@ivitec.com.mx>
2023-01-19 13:29:18 -0600

    v0.25.5 Se integra isn

--- controllers/controlador_adm_session.php
+++ controllers/controlador_adm_session.php
@@ -24,7 +24,7 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
     public array $secciones = array("cat_sat_division_producto","cat_sat_grupo_producto","cat_sat_clase_producto","cat_sat_tipo_producto",
         "cat_sat_producto",'cat_sat_moneda','cat_sat_metodo_pago','cat_sat_forma_pago','cat_sat_unidad','cat_sat_obj_imp',
         'cat_sat_uso_cfdi','cat_sat_regimen_fiscal','cat_sat_tipo_factor','cat_sat_factor','cat_sat_tipo_de_comprobante',
-        'cat_sat_isr','cat_sat_subsidio');
+        'cat_sat_isr','cat_sat_isn','cat_sat_subsidio');
     public array $links_catalogos = array();
 
     public stdClass $links;
@@ -72,6 +72,8 @@ class controlador_adm_session extends \gamboamartin\controllers\controlador_adm_
         $this->links_catalogos["cat_sat_tipo_de_comprobante"]["subtitulo"] = "Catálogo";
         $this->links_catalogos["cat_sat_isr"]["titulo"] = "ISR";
         $this->links_catalogos["cat_sat_isr"]["subtitulo"] = "Catálogo";
+        $this->links_catalogos["cat_sat_isn"]["titulo"] = "ISN";
+        $this->links_catalogos["cat_sat_isn"]["subtitulo"] = "Catálogo";
         $this->links_catalogos["cat_sat_subsidio"]["titulo"] = "Subsidio";
         $this->links_catalogos["cat_sat_subsidio"]["subtitulo"] = "Catálogo";
     }
